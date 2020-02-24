0e3a99058b1749b1c2b973adfe33bc2d1ca40a03
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-02-24 12:33:52 +0000

    Initial set up

---
+++ README.md
@@ -0,0 +1,13 @@
+# Names to Faces
+<p align="center">
+  <img src="demo/app-icon.png " alt="Names to Faces application icon"
+	  title="Names to Faces application icon" align="center" width="200" height="200" />
+</p>
+</br>
+An application to help store names of people you've met.
+
+## Examples within this code
+...
+
+## Demonstration
+...
---
+++ names-to-faces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  names-to-faces
+//
+//  Created by Bradley Chesworth on 24/02/2020.
+//  Copyright © 2020 Brad Chesworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
